3e9aeb85e3717affaa3ad02838ab375c8b1a0f01
lengors <pedrocavadinhas@gmail.com>
2019-05-06 00:34:32 +0100

    Fixed Errors

--- src/main/java/ua/tqs/projects/individual/controllers/StatisticsController.java
+++ src/main/java/ua/tqs/projects/individual/controllers/StatisticsController.java
@@ -21,209 +21,188 @@ public class StatisticsController
 	@Autowired
 	private StatisticsRepository statisticsRepository;
 
+	static final String CITIES = "cities";
+	
 	@GetMapping(value = "/hits")
 	public Map<String, Object> getHits()
 	{
 		return statisticsRepository.findAll().stream().collect(Collectors.toMap(Statistics::getKey, Statistics::getHits));
 	}
 	
-	@SuppressWarnings("serial")
 	@GetMapping(value = "/hits/sum")
 	public Map<String, Object> getHitsSum()
 	{
-		return new HashMap<String, Object>() {{
-			put("sum", statisticsRepository.findAll().stream().collect(Collectors.summingInt(Statistics::getHits)));
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("sum", statisticsRepository.findAll().stream().collect(Collectors.summingInt(Statistics::getHits)));
+		return map;
 	}
 	
 	@GetMapping(value = "/hits/cities")
 	public Map<String, Object> getHitsCities()
 	{
 		Map<String, Object> map = new HashMap<>();
-		map.put("hits", statisticsRepository.findById("cities").orElseGet(() -> new Statistics()).getHits());
+		map.put("hits", statisticsRepository.findById(CITIES).orElseGet(Statistics::new).getHits());
 		return map;
 	}
 	
 	@GetMapping(value = "/hits/meteorology", params = "id")
 	public Map<String, Object> getHitsById(@RequestParam int id)
 	{
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("hits", statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(() -> new Statistics()).getHits());
+		Map<String, Object> map = new HashMap<>();
+		map.put("hits", statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(Statistics::new).getHits());
 		return map;
 	}
 	
-	@SuppressWarnings("serial")
 	@GetMapping(value = "/hits/meteorology", params = "day")
-	public Map<String, Object> GetHitsByDay(@RequestParam int day)
+	public Map<String, Object> getHitsByDay(@RequestParam int day)
 	{
-		return new HashMap<String, Object>() {{
-			put("hits", statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(() -> new Statistics()).getHits());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("hits", statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(Statistics::new).getHits());
+		return map;
 	}
 	
-	@SuppressWarnings("serial")
 	@GetMapping(value = "/hits/meteorology", params = { "id", "days" })
-	public Map<String, Object> GetHitsByIdDay(@RequestParam int id, @RequestParam int days)
+	public Map<String, Object> getHitsByIdDay(@RequestParam int id, @RequestParam int days)
 	{
-		return new HashMap<String, Object>() {{
-			put("hits", statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(() -> new Statistics()).getHits());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("hits", statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(Statistics::new).getHits());
+		return map;
 	}
 	
 	@GetMapping(value = "/misses")
-	public Map<String, Object> GetMisses()
+	public Map<String, Object> getMisses()
 	{
-		return statisticsRepository.findAll().stream().collect(Collectors.toMap(x -> x.getKey(), x -> x.getMisses()));
+		return statisticsRepository.findAll().stream().collect(Collectors.toMap(Statistics::getKey, Statistics::getMisses));
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/misses/sum")
-	public Map<String, Object> GetMissesSum()
+	public Map<String, Object> getMissesSum()
 	{
-		return new HashMap<String, Object>() {{
-			put("sum", statisticsRepository.findAll().stream().collect(Collectors.summingInt(x -> x.getMisses())));
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("sum", statisticsRepository.findAll().stream().collect(Collectors.summingInt(Statistics::getMisses)));
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/misses/cities")
-	public Map<String, Object> GetMissesCities()
+	public Map<String, Object> getMissesCities()
 	{
-		return new HashMap<String, Object>() {{
-			put("misses", statisticsRepository.findById("cities").orElseGet(() -> new Statistics()).getMisses());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("misses", statisticsRepository.findById(CITIES).orElseGet(Statistics::new).getMisses());
+		return map;
 	}
 	
-	@SuppressWarnings("serial")
 	@GetMapping(value = "/misses/meteorology", params = "id")
-	public Map<String, Object> GetMissesById(@RequestParam int id)
+	public Map<String, Object> getMissesById(@RequestParam int id)
 	{
-		return new HashMap<String, Object>() {{
-			put("misses", statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(() -> new Statistics()).getMisses());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("misses", statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(Statistics::new).getMisses());
+		return map;
 	}
 	
-	@SuppressWarnings("serial")
 	@GetMapping(value = "/misses/meteorology", params = "day")
-	public Map<String, Object> GetMissesByDay(@RequestParam int day)
+	public Map<String, Object> getMissesByDay(@RequestParam int day)
 	{
-		return new HashMap<String, Object>() {{
-			put("misses", statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(() -> new Statistics()).getMisses());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("misses", statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(Statistics::new).getMisses());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/misses/meteorology", params = { "id", "days" })
-	public Map<String, Object> GetMissesByIdDay(@RequestParam int id, @RequestParam int days)
+	public Map<String, Object> getMissesByIdDay(@RequestParam int id, @RequestParam int days)
 	{
-		return new HashMap<String, Object>() {{
-			put("misses", statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(() -> new Statistics()).getMisses());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("misses", statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(Statistics::new).getMisses());
+		return map;
 	}
 
 	@GetMapping(value = "/requests")
-	public Map<String, Object> GetRequests()
+	public Map<String, Object> getRequests()
 	{
-		return statisticsRepository.findAll().stream().collect(Collectors.toMap(x -> x.getKey(), x -> x.getRequests()));
+		return statisticsRepository.findAll().stream().collect(Collectors.toMap(Statistics::getKey, Statistics::getRequests));
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/requests/sum")
-	public Map<String, Object> GetRequestsSum()
+	public Map<String, Object> getRequestsSum()
 	{
-		return new HashMap<String, Object>() {{
-			put("sum", statisticsRepository.findAll().stream().collect(Collectors.summingInt(x -> x.getRequests())));
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("sum", statisticsRepository.findAll().stream().collect(Collectors.summingInt(Statistics::getRequests)));
+		return map;
 	}
 	
-	@SuppressWarnings("serial")
 	@GetMapping(value = "/requests/cities")
-	public Map<String, Object> GetRequestsCities()
+	public Map<String, Object> getRequestsCities()
 	{
-		return new HashMap<String, Object>() {{
-			put("requests", statisticsRepository.findById("cities").orElseGet(() -> new Statistics()).getRequests());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("requests", statisticsRepository.findById(CITIES).orElseGet(Statistics::new).getRequests());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/requests/meteorology", params = "id")
-	public Map<String, Object> GetRequestsById(@RequestParam int id)
+	public Map<String, Object> getRequestsById(@RequestParam int id)
 	{
-		return new HashMap<String, Object>() {{
-			put("requests", statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(() -> new Statistics()).getRequests());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("requests", statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(Statistics::new).getRequests());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/requests/meteorology", params = "day")
-	public Map<String, Object> GetRequestsByDay(@RequestParam int day)
+	public Map<String, Object> getRequestsByDay(@RequestParam int day)
 	{
-		return new HashMap<String, Object>() {{
-			put("requests", statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(() -> new Statistics()).getRequests());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("requests", statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(Statistics::new).getRequests());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/requests/meteorology", params = { "id", "days" })
-	public Map<String, Object> GetRequestsByIdDay(@RequestParam int id, @RequestParam int days)
+	public Map<String, Object> getRequestsByIdDay(@RequestParam int id, @RequestParam int days)
 	{
-		return new HashMap<String, Object>() {{
-			put("requests", statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(() -> new Statistics()).getRequests());
-		}};
+		Map<String, Object> map = new HashMap<>();
+		map.put("requests", statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(Statistics::new).getRequests());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/ratio")
-	public Map<String, Object> GetRatio()
+	public Map<String, Object> getRatio()
 	{
 		List<Statistics> statistics = statisticsRepository.findAll();
-		return new HashMap<String, Object>()
-		{{
-			put("ratio", statistics.stream().collect(Collectors.summingInt(x -> x.getHits())).doubleValue() / statistics.stream().collect(Collectors.summingInt(x -> x.getMisses())).doubleValue());
-		}};
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("ratio", statistics.stream().collect(Collectors.summingInt(x -> x.getHits())).doubleValue() / statistics.stream().collect(Collectors.summingInt(Statistics::getMisses)).doubleValue());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/ratio/cities")
-	public Map<String, Object> GetRatioCities()
+	public Map<String, Object> getRatioCities()
 	{
-		Statistics statistics = statisticsRepository.findById("cities").orElseGet(() -> new Statistics());
-		return new HashMap<String, Object>()
-		{{
-			put("ratio", (double) statistics.getHits() / statistics.getMisses());
-		}};
+		Statistics statistics = statisticsRepository.findById(CITIES).orElseGet(Statistics::new);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("ratio", (double) statistics.getHits() / statistics.getMisses());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/ratio/meteorology", params = "id")
-	public Map<String, Object> GetRatioById(@RequestParam int id)
+	public Map<String, Object> getRatioById(@RequestParam int id)
 	{
-		Statistics statistics = statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(() -> new Statistics());
-		return new HashMap<String, Object>()
-		{{
-			put("ratio", (double) statistics.getHits() / statistics.getMisses());
-		}};
+		Statistics statistics = statisticsRepository.findById(String.format("meteorology?id=%d", id)).orElseGet(Statistics::new);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("ratio", (double) statistics.getHits() / statistics.getMisses());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/ratio/meteorology", params = "day")
-	public Map<String, Object> GetRatioByDay(@RequestParam int day)
+	public Map<String, Object> getRatioByDay(@RequestParam int day)
 	{
-		Statistics statistics = statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(() -> new Statistics());
-		return new HashMap<String, Object>()
-		{{
-			put("ratio", (double) statistics.getHits() / statistics.getMisses());
-		}};
+		Statistics statistics = statisticsRepository.findById(String.format("meteorology?day=%d", day)).orElseGet(Statistics::new);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("ratio", (double) statistics.getHits() / statistics.getMisses());
+		return map;
 	}
-	
-	@SuppressWarnings("serial")
+
 	@GetMapping(value = "/ratio/meteorology", params = { "id", "days" })
-	public Map<String, Object> GetRatioByIdDay(@RequestParam int id, @RequestParam int days)
+	public Map<String, Object> getRatioByIdDay(@RequestParam int id, @RequestParam int days)
 	{
-		Statistics statistics = statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(() -> new Statistics());
-		return new HashMap<String, Object>()
-		{{
-			put("ratio", (double) statistics.getHits() / statistics.getMisses());
-		}};
+		Statistics statistics = statisticsRepository.findById(String.format("meteorology?id=%d&days=%d", id, days)).orElseGet(Statistics::new);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("ratio", (double) statistics.getHits() / statistics.getMisses());
+		return map;
 	}
 }
